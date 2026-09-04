7d0d8e266d6d0edb5a6184ebf8dc9d4cc829fc91
mattbutcher577-bit <mattbutcher577@gmail.com>
2026-09-04 22:03:27 +0100

    test: cover manual restart after F8 stop

--- runelite-client/src/test/java/net/runelite/client/plugins/geautotrader/GeAutoTraderPluginTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/geautotrader/GeAutoTraderPluginTest.java
@@ -2,8 +2,10 @@ package net.runelite.client.plugins.geautotrader;
 
 import java.awt.Canvas;
 import java.awt.event.KeyEvent;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import net.runelite.client.events.ConfigChanged;
 
 public class GeAutoTraderPluginTest
 {
@@ -16,5 +18,35 @@ public class GeAutoTraderPluginTest
 		plugin.keyPressed(event);
 		assertTrue(plugin.isStopped());
 		assertTrue(event.isConsumed());
+	}
+
+	@Test
+	public void testF8RestartRequiresExplicitOffThenOn()
+	{
+		GeAutoTraderPlugin plugin = new GeAutoTraderPlugin();
+		KeyEvent event = new KeyEvent(
+			new Canvas(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_F8, KeyEvent.CHAR_UNDEFINED);
+		plugin.keyPressed(event);
+
+		plugin.onConfigChanged(enabledChange("true"));
+		assertFalse(plugin.isRestartRequested());
+		assertTrue(plugin.isStopped());
+
+		plugin.onConfigChanged(enabledChange("false"));
+		assertFalse(plugin.isRestartRequested());
+		assertTrue(plugin.isStopped());
+
+		plugin.onConfigChanged(enabledChange("true"));
+		assertTrue(plugin.isRestartRequested());
+		assertTrue(plugin.isStopped());
+	}
+
+	private static ConfigChanged enabledChange(String value)
+	{
+		ConfigChanged event = new ConfigChanged();
+		event.setGroup(GeAutoTraderPlugin.CONFIG_GROUP);
+		event.setKey("enabled");
+		event.setNewValue(value);
+		return event;
 	}
 }
