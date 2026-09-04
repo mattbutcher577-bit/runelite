c697efebe245a6d9c77524206abc5c3cc5081cc4
mattbutcher577-bit <mattbutcher577@gmail.com>
2026-09-04 17:06:41 +0100

    feat: publish exact GE setup values

--- runelite-client/src/main/java/net/runelite/client/plugins/gebridge/GeBridgeGeState.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/gebridge/GeBridgeGeState.java
@@ -8,7 +8,52 @@ class GeBridgeGeState
 	boolean open;
 	boolean offerSetupOpen;
 	int offerSetupItemId;
+	int offerSetupQuantity;
+	int offerSetupPrice;
+	String offerSetupType;
 	GeBridgeBounds windowBounds;
 	GeBridgeBounds offerSetupBounds;
 	GeBridgeBounds inventoryBounds;
+
+	GeBridgeGeState(
+		boolean open,
+		boolean offerSetupOpen,
+		int offerSetupItemId,
+		int offerSetupQuantity,
+		int offerSetupPrice,
+		String offerSetupType,
+		GeBridgeBounds windowBounds,
+		GeBridgeBounds offerSetupBounds,
+		GeBridgeBounds inventoryBounds)
+	{
+		this.open = open;
+		this.offerSetupOpen = offerSetupOpen;
+		this.offerSetupItemId = offerSetupItemId;
+		this.offerSetupQuantity = offerSetupQuantity;
+		this.offerSetupPrice = offerSetupPrice;
+		this.offerSetupType = offerSetupType == null ? "UNKNOWN" : offerSetupType;
+		this.windowBounds = windowBounds;
+		this.offerSetupBounds = offerSetupBounds;
+		this.inventoryBounds = inventoryBounds;
+	}
+
+	GeBridgeGeState(
+		boolean open,
+		boolean offerSetupOpen,
+		int offerSetupItemId,
+		GeBridgeBounds windowBounds,
+		GeBridgeBounds offerSetupBounds,
+		GeBridgeBounds inventoryBounds)
+	{
+		this(
+			open,
+			offerSetupOpen,
+			offerSetupItemId,
+			0,
+			0,
+			"UNKNOWN",
+			windowBounds,
+			offerSetupBounds,
+			inventoryBounds);
+	}
 }
