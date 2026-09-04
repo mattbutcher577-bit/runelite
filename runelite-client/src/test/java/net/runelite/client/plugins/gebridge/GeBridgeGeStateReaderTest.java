55cc13709bc2aa280981556409bba4555fc4828d
mattbutcher577-bit <mattbutcher577@gmail.com>
2026-09-04 17:14:26 +0100

    test: read exact GE setup varbits

---
+++ runelite-client/src/test/java/net/runelite/client/plugins/gebridge/GeBridgeGeStateReaderTest.java
@@ -0,0 +1,45 @@
+package net.runelite.client.plugins.gebridge;
+
+import java.awt.Rectangle;
+import net.runelite.api.Client;
+import net.runelite.api.gameval.VarPlayerID;
+import net.runelite.api.gameval.VarbitID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GeBridgeGeStateReaderTest
+{
+	@Test
+	public void testReadsExactBuySetupValues()
+	{
+		Client client = mock(Client.class);
+		GeBridgeInterfaceState interfaces = new GeBridgeInterfaceState(
+			true, true, false, false, false, false, false);
+
+		when(client.getVarpValue(VarPlayerID.TRADINGPOST_SEARCH)).thenReturn(314);
+		when(client.getVarbitValue(VarbitID.GE_NEWOFFER_QUANTITY)).thenReturn(1000);
+		when(client.getVarbitValue(VarbitID.GE_NEWOFFER_PRICE)).thenReturn(12);
+		when(client.getVarbitValue(VarbitID.GE_NEWOFFER_TYPE)).thenReturn(0);
+		when(client.getWidget(WidgetInfo.GRAND_EXCHANGE_WINDOW_CONTAINER)).thenReturn(widget(10, 20, 500, 350));
+		when(client.getWidget(WidgetInfo.GRAND_EXCHANGE_OFFER_CONTAINER)).thenReturn(widget(30, 50, 420, 250));
+		when(client.getWidget(WidgetInfo.GRAND_EXCHANGE_INVENTORY_ITEMS_CONTAINER)).thenReturn(widget(550, 100, 180, 250));
+
+		GeBridgeGeState state = GeBridgeGeStateReader.read(client, interfaces);
+		assertEquals(314, state.getOfferSetupItemId());
+		assertEquals(1000, state.getOfferSetupQuantity());
+		assertEquals(12, state.getOfferSetupPrice());
+		assertEquals("BUY", state.getOfferSetupType());
+	}
+
+	private static Widget widget(int x, int y, int width, int height)
+	{
+		Widget widget = mock(Widget.class);
+		when(widget.isHidden()).thenReturn(false);
+		when(widget.getBounds()).thenReturn(new Rectangle(x, y, width, height));
+		return widget;
+	}
+}
