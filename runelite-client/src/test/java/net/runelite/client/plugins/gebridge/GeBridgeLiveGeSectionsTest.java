146a8eb81da9cb84425cefbfc1e083eb644fa22a
mattbutcher577-bit <mattbutcher577@gmail.com>
2026-09-05 00:15:02 +0100

    test: cover bridge live item-search diagnostics

--- runelite-client/src/test/java/net/runelite/client/plugins/gebridge/GeBridgeLiveGeSectionsTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/gebridge/GeBridgeLiveGeSectionsTest.java
@@ -1,10 +1,13 @@
 package net.runelite.client.plugins.gebridge;
 
 import net.runelite.api.vars.InputType;
+import net.runelite.api.widgets.Widget;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GeBridgeLiveGeSectionsTest
 {
@@ -27,5 +30,28 @@ public class GeBridgeLiveGeSectionsTest
 		assertNotNull(sections.getGeActions());
 		assertNotNull(sections.getGeInventory());
 		assertFalse(sections.getGeInventory().isOpen());
+	}
+
+	@Test
+	public void testLiveSectionsUseVisibleInputFallbackForUnknownBuySearch()
+	{
+		Widget inputField = mock(Widget.class);
+		when(inputField.isHidden()).thenReturn(false);
+
+		GeBridgeLiveGeSections sections = GeBridgeLiveGeSections.read(
+			true,
+			99,
+			null,
+			inputField,
+			true,
+			-1,
+			null,
+			null,
+			null,
+			null,
+			null,
+			78L);
+
+		assertEquals("ITEM_SEARCH", sections.getGeInput().getMode());
 	}
 }
