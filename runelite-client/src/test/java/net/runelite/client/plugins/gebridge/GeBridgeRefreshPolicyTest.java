31df13b0d05d420a5fdc7f98f826c1c4d16c0b16
mattbutcher577-bit <mattbutcher577@gmail.com>
2026-09-04 16:29:49 +0100

    test: define event driven protocol 5 refresh policy

---
+++ runelite-client/src/test/java/net/runelite/client/plugins/gebridge/GeBridgeRefreshPolicyTest.java
@@ -0,0 +1,34 @@
+package net.runelite.client.plugins.gebridge;
+
+import net.runelite.api.ScriptID;
+import net.runelite.api.gameval.VarClientID;
+import net.runelite.api.widgets.WidgetInfo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class GeBridgeRefreshPolicyTest
+{
+	@Test
+	public void testGeSearchAndOfferSetupScriptsRefreshState()
+	{
+		assertTrue(GeBridgeRefreshPolicy.shouldRefreshScript(ScriptID.GE_ITEM_SEARCH));
+		assertTrue(GeBridgeRefreshPolicy.shouldRefreshScript(ScriptID.GE_OFFERS_SETUP_BUILD));
+		assertFalse(GeBridgeRefreshPolicy.shouldRefreshScript(-1));
+	}
+
+	@Test
+	public void testMessageLayerModeRefreshesSemanticInputState()
+	{
+		assertTrue(GeBridgeRefreshPolicy.shouldRefreshVarClient(VarClientID.MESLAYERMODE));
+		assertFalse(GeBridgeRefreshPolicy.shouldRefreshVarClient(-1));
+	}
+
+	@Test
+	public void testOnlyGrandExchangeWidgetGroupRefreshesGeState()
+	{
+		int geGroup = WidgetInfo.GRAND_EXCHANGE_WINDOW_CONTAINER.getGroupId();
+		assertTrue(GeBridgeRefreshPolicy.shouldRefreshWidgetGroup(geGroup));
+		assertFalse(GeBridgeRefreshPolicy.shouldRefreshWidgetGroup(geGroup + 1));
+	}
+}
