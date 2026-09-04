18f3c7e52cdd2fcde3798d15eac8b2decea41ea1
mattbutcher577-bit <mattbutcher577@gmail.com>
2026-09-04 23:00:19 +0100

    test: cover Create Buy offer dispatch target

--- runelite-client/src/test/java/net/runelite/client/plugins/geautotrader/GeActionDispatcherTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/geautotrader/GeActionDispatcherTest.java
@@ -1,13 +1,14 @@
 package net.runelite.client.plugins.geautotrader;
 
 import java.awt.Canvas;
+import java.util.Collections;
 import net.runelite.api.Client;
 import net.runelite.api.MenuAction;
 import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -58,5 +59,60 @@ public class GeActionDispatcherTest
 			-1,
 			"Select",
 			"Adamant platebody");
+	}
+
+	@Test
+	public void testOpenBuyRecognizesCreateBuyOfferAction()
+	{
+		Client client = mock(Client.class);
+		Widget window = mock(Widget.class);
+		Widget buy = mock(Widget.class);
+
+		when(client.getWidget(WidgetInfo.GRAND_EXCHANGE_WINDOW_CONTAINER)).thenReturn(window);
+		when(window.isHidden()).thenReturn(false);
+		when(window.getActions()).thenReturn(null);
+		when(window.getChildren()).thenReturn(new Widget[]{buy});
+
+		when(buy.isHidden()).thenReturn(false);
+		when(buy.getActions()).thenReturn(new String[]{"Create Buy offer"});
+		when(buy.getIndex()).thenReturn(7);
+		when(buy.getId()).thenReturn(54321);
+		when(buy.getItemId()).thenReturn(-1);
+		when(buy.getName()).thenReturn("");
+
+		GeExecutionService execution = new GeExecutionService(client, () -> false);
+		GePromptInputService input = new GePromptInputService(new Canvas(), () -> false);
+		GeActionDispatcher dispatcher = new GeActionDispatcher(client, execution, input);
+
+		GeObservedState state = new GeObservedState(
+			true,
+			false,
+			true,
+			true,
+			false,
+			2_000_000L,
+			Collections.singletonList(new GeObservedSlot(1, "EMPTY", -1, 0, 0, 0)),
+			-1,
+			0,
+			0,
+			GeTradeSide.UNKNOWN);
+		GePlannedAction action = GePlannedAction.of(
+			GePlannedActionType.OPEN_BUY,
+			1,
+			1127,
+			"Adamant platebody",
+			125,
+			9001,
+			"v6-buy-1");
+
+		assertEquals(GeReasonCode.OK, dispatcher.dispatch(action, state));
+		verify(client).menuAction(
+			7,
+			54321,
+			MenuAction.CC_OP,
+			1,
+			-1,
+			"Create Buy offer",
+			"");
 	}
 }
