659fcfab586c3df772e4404f979f90890c872513
mattbutcher577-bit <mattbutcher577@gmail.com>
2026-09-04 15:13:31 +0100

    feat: add GE slot action bounds model

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/gebridge/GeBridgeGeActionSlot.java
@@ -0,0 +1,12 @@
+package net.runelite.client.plugins.gebridge;
+
+import lombok.Value;
+
+@Value
+class GeBridgeGeActionSlot
+{
+	int slot;
+	GeBridgeBounds slotBounds;
+	GeBridgeBounds buyButton;
+	GeBridgeBounds sellButton;
+}
